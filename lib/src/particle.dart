7af1b778f229026f6efe2b4a1ee7361510c77164
Masayuki Ono <mono0926@gmail.com>
2019-11-30 18:46:25 +0900

    Format

--- lib/src/particle.dart
+++ lib/src/particle.dart
@@ -1,10 +1,9 @@
 import 'dart:math';
 
+import 'package:confetti/src/helper.dart';
 import 'package:flutter/material.dart';
 import 'package:random_color/random_color.dart';
 import 'package:vector_math/vector_math.dart' as vmath;
-
-import 'package:confetti/src/helper.dart';
 
 enum ParticleSystemStatus {
   started,
